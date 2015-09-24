f39129017b212c71762cd6f097722413a0f92e8a
Olly Banham <oliver.banham@adgistics.com>
2015-09-24 12:45:10 +0100

    pull through image for bootstrap modals on image library

--- javascripts/main.js
+++ javascripts/main.js
@@ -74,4 +74,13 @@ function(){
 $(".share-new-text").hide();
 });
 
+$('#shareModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var image = button.data('image') // Extract info from data-* attributes
+
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.img-solo').attr('src', image);
+})
+
 });
