fb3ad26563868d559f826244232b9392d945eea8
unknown <obanham@obanham.adgistics.com>
2015-10-21 15:31:08 +0100

    Added Bootstrap tour, drag functionality on dropdown, and close sidebar functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
     // Init
     $('.alt-checkbox input, .alt-radio input:checked').change();
 
-    $(function () {
+
   $('[data-toggle="popover"]').popover()
 
   $(".icon-add").on("click", function(){
@@ -62,16 +62,20 @@ $(".js-toggle-images").hide();
     $(this).next(".js-tasks").next(".js-tasks").toggle();
   });
 
-    $(".sortable").sortable();
+$(".sortable").sortable();
 
-});
+   $( "#quick-access-sortable, .apps-sortable" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
 
-$(".share-new-text").hide();
+var shareNewText = $(".share-new-text")
+
+shareNewText.hide();
 $(".share-new").hover( function(){
-$(".share-new-text").show('slow');
+  shareNewText.show('slow');
 },
 function(){
-$(".share-new-text").hide();
+shareNewText.hide();
 });
 
 $('#shareModal').on('show.bs.modal', function (event) {
@@ -81,6 +85,102 @@ $('#shareModal').on('show.bs.modal', function (event) {
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
   modal.find('.img-solo').attr('src', image);
-})
+});
+
+// Close widget sidebar
+$(".js-close-widgets").on("click", function(){
+  var self = $(this);
+  $(".js-widget").toggle();
+  self.find("span").toggleClass("icon-fast-forward").toggleClass("icon-rewind");
+  $(".js-widget-column").toggleClass("col-md-4").toggleClass("col-md-2");
+  $(".js-main-content").toggleClass("col-md-offset-2");
+});
+
+
+// The Tour!
+var tour = new Tour({
+  storage: false,
+  steps: [
+  {
+    element: ".js-start-tour",
+    title: "Welcome to Brandzone",
+    content: "Welcome to your Brandzone, Matthew. Let us show you around the place.",
+    backdrop: true,
+    placement: "bottom"
+  },
+  {
+    element: ".tour-home",
+    title: "Going home",
+    content: "You're on the homepage right now. If you venture away and want to return, click the icon",
+    backdrop: true
+  },
+  {
+    element: ".tour-myapps",
+    title: "The App menu",
+    content: "This is your app menu. Drag to rearrange their order.",
+    backdrop: true,
+    placement: "bottom"
+  },
+  {
+    element: ".quick-tour",
+    title: "Quick access apps",
+    content: "Drag apps from the menu to this area to have easy access to your favourites",
+    backdrop: true,
+    placement: "bottom"
+  },
+  {
+    element: ".notifications-icon",
+    title: "Notifications",
+    content: "View all your latest notications here. Never miss a thing",
+    backdrop: true,
+    placement: "bottom"
+  },
+  {
+    element: ".tour-share",
+    title: "Sharing",
+    backdrop: true,
+    content: "See a piece of news, an image, or an asset that you want people to know about? Hit this button, fill in the modal, and you're on your way."
+  },
+  {
+    element: ".js-widget-column",
+    title: "My Widgets",
+    backdrop: true,
+    content: "These are your widgets. Add a new one by lorem ipsum. Drag to rearrange them. Choose to hide them by clicking the arrows. Don't use a widget anymore? Drag it into the recyling bin",
+    placement: "left"
+  },
+  {
+      element: ".workspaces",
+      title: "Workspaces",
+      backdrop: true,
+      content: "Workspaces is the place to share files, ideas, in one place, lorem ipsum",
+      placement: "left"
+  },
+    {
+      element: ".my-tasks",
+      title: "My Tasks",
+      backdrop: true,
+      content: "View all your latest tasks here",
+      placement: "left"
+  },
+    {
+      element: ".my-approvals",
+      title: "My Approvals",
+      backdrop: true,
+      content: "View all your latest approvals here",
+      placement: "left"
+  }
+]});
+
+// Initialize the tour
+tour.init();
+
+$(".js-start-tour").on("click", function(e){
+e.preventDefault();
+// Start the tour
+tour.start();
+});
+
+
+
 
 });
